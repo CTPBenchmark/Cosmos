64d911ebd7720e5d2d44c95496f9717d12137f01
Valentin Charbonnier <valentinbreiz@gmail.com>
2017-05-14 18:15:14 +0200

    Add files via upload

--- install-VS2015.bat
+++ install-VS2015.bat
@@ -1,4 +1,22 @@
-@REM This file is used to build the Cosmos sources.
+@echo off
+echo  a88888b.
+echo d8'   `88
+echo 88        .d8888b. .d8888b. 88d8b.d8b. .d8888b. .d8888b.
+echo 88        88'  `88 Y8ooooo. 88'`88'`88 88'  `88 Y8ooooo.
+echo Y8.   .88 88.  .88       88 88  88  88 88.  .88       88
+echo  Y88888P' `88888P' `88888P' dP  dP  dP `88888P' `88888P' operating system construction kit
+echo.
+echo Bienvenue sur le programme d'installation de Cosmos !
+echo Modifie par valentinbreiz pour customprotocol.com/tags/cosmos
+echo.
+echo Pre-requis:
+echo - Windows 7 ou plus
+echo - Visual Studio 2015
+echo - VMWare
+echo - .NET Framework 4.5 
+echo - .NET Framework 3.5 SP1
+echo.
+pause
 
 cls
 del source\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.*
